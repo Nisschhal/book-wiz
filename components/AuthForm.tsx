f3b7d821f0d7cf670d0453c8d69cb7cc6ce086ef
nischal puri <mrnischalpuri@gmail.com>
2025-01-23 10:39:37 +0545

    [auth form UI created]

---
+++ components/AuthForm.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  DefaultValues,
+  UseFormReturn,
+  SubmitHandler,
+  FieldValues,
+  useForm,
+  Path,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
+import ImageUpload from "./ImageUpload";
+
+interface Props<T extends FieldValues> {
+  schema: z.ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const isSignin = type === "SIGN_IN";
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignin ? "Welcome Back to BookWiz" : "Create Your Library Account"}
+      </h1>
+      <p className="text-light-100">
+        {isSignin
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all fields and upload a valid university ID to gain access to the library"}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {/* // get the key out of field_name type */}
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    {field.name === "universityCard" ? (
+                      <ImageUpload />
+                    ) : (
+                      <Input
+                        className="form-input"
+                        type={
+                          FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]
+                        }
+                        placeholder="shadcn"
+                        {...field}
+                      />
+                    )}
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button type="submit" className="form-btn">
+            {isSignin ? "Sign in" : "Sign up"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium">
+        {isSignin ? "New to BookWiz? " : "Already have an account? "}
+        <Link
+          href={isSignin ? "/signup" : "/signin"}
+          className="font-bold text-primary"
+        >
+          {isSignin ? "Create an account" : "Sign in"}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ lib/validations.ts
@@ -0,0 +1,16 @@
+import { University } from "lucide-react";
+import { z } from "zod";
+export const signUpSchema = z.object({
+  fullName: z.string().min(3),
+  email: z.string().email(),
+  universityId: z.coerce.number(),
+  universityCard: z.string().nonempty("Univerisity Card is required"),
+  password: z.string().min(8),
+});
+
+export const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export type AUTHTYPE = "SIGN_IN" | "SIGN_UP";
